d139b4d9fd8bd23a87fc574948d59275a58dbb21
serguei3000 <serguei3000@yandex.ru>
2020-06-10 07:24:44 +0300

    Update MainController.java

--- InsurenceServer/src/main/java/south62/com/controllers/MainController.java
+++ InsurenceServer/src/main/java/south62/com/controllers/MainController.java
@@ -13,7 +13,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
+//import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 import south62.com.entity.Agreement;
 import south62.com.entity.Client;
